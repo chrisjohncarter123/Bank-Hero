1b1b3634faf64e2c14e17db73994ad80bfc4ba5e
github username <github email address>
2020-07-13 03:45:36 -0500

    fetchAccounts updated

--- front-end/src/actions/fetchAccounts.js
+++ front-end/src/actions/fetchAccounts.js
@@ -1,10 +1,10 @@
 export const fetchAccounts = () => {
     return (dispatch) => {
-      dispatch({ type: 'LOADING_CATS'})
-      fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(response => {
+      dispatch({ type: 'LOADING_ACCOUNTS'})
+      fetch('https://localhost:4000/accounts').then(response => {
         return response.json()
       }).then(responseJSON => {
-        dispatch({ type: 'ADD_CATS', cats: responseJSON.images })
+        dispatch({ type: 'ADD_ACCOUNTS', accounts: responseJSON.json })
       })
     }
   }
