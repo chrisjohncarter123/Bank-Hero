7e5ad8d1bf7c26ece54488446bc43aafcc06e8db
github username <github email address>
2020-07-15 02:44:36 -0500

    Updated footer

--- front-end/src/components/footer.jsx
+++ front-end/src/components/footer.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-function Footer() {
-  return (
-    <div class="footer">
-      <p>thank you for using Bank-Hero!</p>
-      <p className="mt-5 mb-3 text-muted">&copy; 2020</p>
-    </div>
-  );
+class Footer {
+  render() {
+    return (
+      <div class="footer">
+        <p>thank you for using Bank-Hero!</p>
+        <p className="mt-5 mb-3 text-muted">&copy; </p>
+      </div>
+    );
+  }
 }
 
 export default Footer;
