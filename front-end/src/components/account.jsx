80b0fd2e050637c10b782a7154ef30851f81266e
github username <github email address>
2020-07-14 23:38:42 -0500

    Updated account.jsx

--- front-end/src/components/account.jsx
+++ front-end/src/components/account.jsx
@@ -14,8 +14,7 @@ class Account extends Component {
       };
     }
     return (  
-      <StoreContext.Consumer>
-        {storeContext => 
+      <div>
           <div className="account">
             
             <h3> </h3>
@@ -23,9 +22,8 @@ class Account extends Component {
             <p><b>Created At: </b>{this.props.created_at}</p>
             <p><b>Cash: </b><span style={cashStyle}>{this.props.cash}</span></p>
 
-          </div>
-        }
-        </StoreContext.Consumer>
+          </div>  
+        </div>
     );
   }
 }
