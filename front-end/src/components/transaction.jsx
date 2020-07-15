966a7c0f3010f03e4f530fecc2b6953eeb81d286
github username <github email address>
2020-07-14 21:41:08 -0500

    Transactions

---
+++ front-end/src/components/transaction.jsx
@@ -0,0 +1,42 @@
+
+import React, { Component } from 'react';
+import StoreContext from '../contexts/storeContext';
+
+
+class Transaction extends Component {
+  state = { 
+    name: "Bank of World",
+    cash: "1234.43",
+    tags:["tag1", "tag2", "tag3"]
+   }
+
+   styles = {
+     fontWeight: "bold"
+   }
+
+   handleName = product => {
+     console.log(product);
+     console.log('Clicked', );
+   }
+   //{storeContext.name}
+   //{this.store.name}
+
+  render() { 
+    return (  
+      <StoreContext.Consumer>
+        {storeContext => 
+          <div className="account">
+            
+            <h3 style={this.styles}> </h3>
+            <p><b>Account Name: </b>{this.props.name}</p>
+            <p><b>Cash: </b>{this.props.cash}</p>
+
+          </div>
+        }
+        </StoreContext.Consumer>
+    );
+  }
+}
+ 
+export default Transaction;
+
