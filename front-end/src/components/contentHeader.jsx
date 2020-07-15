7b6e627e63787963564c7d48f6526b206226ca86
github username <github email address>
2020-07-14 21:45:06 -0500

    Added ContentHeader

---
+++ front-end/src/components/contentHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ContentHeader() {
+  return (
+    <div className="contentHeader">
+        <br />
+            <h1>{this.props.title}</h1>
+        <br />
+    </div>
+  );
+}
+
+export default ContentHeader;
