4ebc892bf8151d8d6a53495bf19850418d28e264
Chris Carter <chrisjohncarter123@gmail.com>
2020-09-19 22:39:57 -0500

    Added UserProfileBadge

---
+++ front-end/src/components/userProfileBadge.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Link
+} from "react-router-dom";
+
+function UserProfileBadge() {
+  return (
+    <div align="center" className="header">
+        <p>Profile Badge</p>
+    </div>
+  );
+}
+
+export default UserProfileBadge;
