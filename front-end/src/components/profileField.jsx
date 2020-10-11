29cdc5cbb899aa836691458ee0a19632e302f11d
Chris Carter <chrisjohncarter123@gmail.com>
2020-10-11 02:30:58 -0500

    Created profileField component

---
+++ front-end/src/components/profileField.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+class ProfileField extends Component {
+  state = { 
+
+   }
+
+   styles = {
+
+   }
+
+  render() { 
+    return (  
+        <div>
+            
+      </div>
+    );
+  }
+}
+ 
+export default ProfileField;
+
