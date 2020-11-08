5fb4e3bf37fe960f41351ac8299741427a4566ea
Chris Carter <chrisjohncarter123@gmail.com>
2020-11-07 19:46:28 -0600

    Updated sessions_controller#create

--- back-end/app/controllers/sessions_controller.rb
+++ back-end/app/controllers/sessions_controller.rb
@@ -3,6 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+
+    @user = User.create(params.require(:user).permit(:username,        
+    :password))
+    session[:user_id] = @user.id
+    redirect_to '/welcome'
+
   end
 
   def login
