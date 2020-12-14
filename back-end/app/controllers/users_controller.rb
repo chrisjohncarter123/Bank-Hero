4e26ce4a77fa3d48fc7227e41e75a1f3e75179ed
Chris Carter <chrisjohncarter123@gmail.com>
2020-12-13 20:12:13 -0600

    Updated passwords

--- back-end/app/controllers/users_controller.rb
+++ back-end/app/controllers/users_controller.rb
@@ -70,6 +70,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:username, :password, :first_name, :last_name, :email)
+      params.require(:user).permit(:username, :password)
     end
 end
--- back-end/db/schema.rb
+++ back-end/db/schema.rb
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2020_07_02_231253) do
     t.string "last_name"
     t.string "email"
     t.string "username"
-    t.string "message_digest"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
