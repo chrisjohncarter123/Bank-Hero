76df52568a9b91cd279a844575798779084122e1
Chris Carter <chrisjohncarter123@gmail.com>
2021-01-17 22:40:51 -0600

    Added BlogPost

---
+++ front-end/src/components/blogPost.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class BlogPost extends Component {
+
+  render() { 
+    
+    return (  
+      <div>
+            <h1>Title</h1>
+
+            <p>Content</p>
+        </div>
+    );
+  }
+}
+ 
+export default BlogPost;
+
